04beb6673f26c2ebe47846cf4548228bd7eb56be
atlength <738030501@qq.com>
2020-08-31 22:20:06 +0800

    success

--- root2.py
+++ root2.py
@@ -1,2 +1,15 @@
-a = 2
-b = 5
+def jisuan(x,y):
+    print ('The value of root of 2 approximately is:  %f' % (y/x-1))
+    x,y=y,x+2*y
+    return x,y
+
+
+def main():
+    a = 2
+    b = 5
+    for i in range(10):
+        a,b = jisuan(a,b)
+
+
+if __name__ == '__main__':
+    main()
